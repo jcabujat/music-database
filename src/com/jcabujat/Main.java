172d9e22c657827feb622db602133c2e0c446c6b
jonathancabujat <jonathancabujat@gmail.com>
2020-10-21 12:18:37 +0800

    01: Code on how to properly query tables using a data source model.

---
+++ src/com/jcabujat/Main.java
@@ -0,0 +1,44 @@
+package com.jcabujat;
+
+import com.jcabujat.model.Artist;
+import com.jcabujat.model.DataSource;
+import com.jcabujat.model.SongArtist;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DataSource dataSource = new DataSource();
+        if (!dataSource.open()) {
+            System.out.println("Can't connect to database.");
+            return;
+        }
+
+//        List<Artist> artistList = dataSource.queryArtists(DataSource.ORDER_BY.NONE);
+//        if (artistList == null) {
+//            System.out.println("No artists!");
+//            return;
+//        }
+//
+//        for (Artist artist : artistList) {
+//            System.out.println("Id = " + artist.getId() + ", Name = " + artist.getName());
+//        }
+
+//        List<String> albums = dataSource.queryAlbumsForArtist("Pink Floyd", DataSource.ORDER_BY.DESC);
+//        if (albums != null) {
+//            for (String album : albums) {
+//                System.out.println(album);
+//            }
+//        }
+
+        List<SongArtist> songArtists = dataSource.queryArtistsBySong("Go Your Own Way", DataSource.ORDER_BY.ASC);
+        if (songArtists != null) {
+            for (SongArtist songArtist : songArtists) {
+                System.out.println(songArtist.getArtistName() + ", " + songArtist.getAlbumName() + ", " + songArtist.getSongTrack());
+            }
+        }
+
+        dataSource.close();
+    }
+}
---
+++ src/com/jcabujat/model/SongArtist.java
@@ -0,0 +1,31 @@
+package com.jcabujat.model;
+
+public class SongArtist {
+    private String artistName;
+    private String albumName;
+    private int songTrack;
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public int getSongTrack() {
+        return songTrack;
+    }
+
+    public void setSongTrack(int songTrack) {
+        this.songTrack = songTrack;
+    }
+}
